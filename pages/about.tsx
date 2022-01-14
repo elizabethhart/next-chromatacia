de3643d3d657e9182ff62a53eba016ead170a13a
elizabethhart <elizabethwhart@gmail.com>
2022-01-13 21:36:18 -0600

    Add prettier

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,8 +1,8 @@
-import { NextPage } from "next";
+import { NextPage } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 const About: NextPage = () => {
-  return <h1>About</h1>;
+  return <h1>{'About'}</h1>;
 };
 
 export async function getStaticProps({ locale }) {
