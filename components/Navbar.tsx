da0d693c3e9030a18bf0bc4f39dbb75fc337a6f7
elizabethhart <elizabethwhart@gmail.com>
2022-06-20 12:11:01 -0500

    WIP

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -19,16 +19,16 @@ const Navbar = () => {
   const { t } = useTranslation();
   const [showMenu, setShowMenu] = useState<boolean>(false);
   const menuOptions: MenuOption[] = [
-    {
-      href: '/gallery',
-      name: t`gallery`,
-      icon: <PhotographIcon className="h-5 w-5 text-slate-500" />,
-    },
-    {
-      href: '/bookshelf',
-      name: t`bookshelf`,
-      icon: <BookOpenIcon className="h-5 w-5 text-slate-500" />,
-    },
+    // {
+    //   href: '/gallery',
+    //   name: t`gallery`,
+    //   icon: <PhotographIcon className="h-5 w-5 text-slate-500" />,
+    // },
+    // {
+    //   href: '/bookshelf',
+    //   name: t`bookshelf`,
+    //   icon: <BookOpenIcon className="h-5 w-5 text-slate-500" />,
+    // },
     {
       href: '/about',
       name: t`about`,
